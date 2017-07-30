b0c12ae5899e0654a37c9a56266f9538ca24dfa5
GRECO, FRANK <frankgreco@northwesternmutual.com>
2017-07-30 15:18:13 -0500

    updating ci

---
+++ travis/build-docker-images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+if [[ "$TRAVIS_SECURE_ENV_VARS" == "false" ]]; then
+  echo "skip docker upload, TRAVIS_SECURE_ENV_VARS=$TRAVIS_SECURE_ENV_VARS"
+  exit 0
+fi
+
+export DOCKER_REPO="northwesternmutual/kanali"
+
+if [[ $TRAVIS_TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "uploading docker image TAG=$TRAVIS_TAG"
+  export DOCKER_TAG=$TRAVIS_TAG
+  bash ./upload-to-docker.sh
+elif [[ $TRAVIS_BRANCH == "master" ]]; then
+  echo "uploading docker image BRANCH=$TRAVIS_BRANCH"
+  for component in latest ${TRAVIS_COMMIT::8}
+  do
+    export DOCKER_TAG=${component}
+    bash ./upload-to-docker.sh
+  done
+else
+  echo "skip docker upload - neither master branch nor tag"
+  exit 0
+fi
---
+++ travis/upload-to-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+if [[ "$DOCKER_REPO" == "" ]]; then
+  echo "skip docker upload, DOCKER_REPO=$DOCKER_REPO"
+  exit 0
+fi
+
+if [[ "$DOCKER_TAG" == "" ]]; then
+  echo "skip docker upload, DOCKER_TAG=$DOCKER_TAG"
+  exit 0
+fi
+
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker tag $DOCKER_REPO:$DOCKER_TAG
+docker push $DOCKER_REPO:$DOCKER_TAG
