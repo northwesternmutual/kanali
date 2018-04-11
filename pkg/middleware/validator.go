e6b6423aa15233f2ec3d8dc4aac9c7f9227c5d2b
GRECO, FRANK <fbgrecojr@me.com>
2018-04-11 14:33:58 -0500

    validator changes

--- pkg/middleware/validator.go
+++ pkg/middleware/validator.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2018 Northwestern Mutual.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package middleware
 
 import (
@@ -34,9 +54,11 @@ func Validator(i versioned.Interface) http.HandlerFunc {
 			return
 		}
 
+		review.Response = new(v1beta1.AdmissionResponse)
+
 		if err := validate.New(r.Context(), i).IsValidResource(review.Request.Kind, review.Request.Object.Raw); err != nil {
 			review.Response.Result = &metav1.Status{
-				Status:  metav1.StatusSuccess,
+				Status:  metav1.StatusFailure,
 				Message: err.Error(),
 			}
 			logger.Info(review.Response.Result.String())
