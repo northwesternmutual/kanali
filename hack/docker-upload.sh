df9861d820b811becf3ca612e6256955a678da5d
GRECO, FRANK <fbgrecojr@me.com>
2018-04-15 19:50:18 -0500

    adding docker builds to travis ci

--- hack/docker-upload.sh
+++ hack/docker-upload.sh
@@ -5,7 +5,7 @@ source hack/vars.sh
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS $IMAGE_REGISTRY
 
-if [ -z "$IMAGE_LATEST" ]; then
+if [ ! -z "$IMAGE_LATEST" ]; then
   docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_NAME:latest
   docker push $IMAGE_NAME:latest
 fi
