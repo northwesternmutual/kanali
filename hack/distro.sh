ea2364b5892550584ecf5e52d6293fd4b68b29e5
Frank B Greco Jr <frankgreco@northwesternmutual.com>
2018-04-20 15:01:30 -0500

    Bug fix (#103)
    * making distro executable
    
    * bug fix
    
    * bug fix
    
    * bug fix

--- hack/distro.sh
+++ hack/distro.sh
@@ -28,16 +28,15 @@ for distro in ${DISTRIBUTIONS[@]}
 do
     remote_path="${S3_BASE_PATH}/${RELEASE}/${distro}/${PROJECT_NAME}ctl"
     raw_binary_name="${PROJECT_NAME}ctl_${distro}"
-    actual_delimiter="\/"
-    desired_delimiter="_"
-
-    echo "gox -osarch=${distro} ${PATH_IMPORT}/cmd/kanalictl"
+    actual_delimiter=\\/
+    desired_delimiter=_
+    gox_binary_name=$(echo "${raw_binary_name}" | sed s/$actual_delimiter/$desired_delimiter/)
 
     gox -osarch=$distro ${PATH_IMPORT}/cmd/kanalictl
 
     if [[ $distro = *"windows"* ]]; then
-      aws s3 mv "${${raw_binary_name}.exe/${actual_delimiter}/${desired_delimiter}}" ${remote_path}.exe
+      aws s3 mv ${gox_binary_name} ${remote_path}.exe
     else
-      aws s3 mv "${${raw_binary_name}/${actual_delimiter}/${desired_delimiter}}" ${remote_path}
+      aws s3 mv ${gox_binary_name} ${remote_path}
     fi
 done
