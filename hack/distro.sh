75bd6d499577a258b35ca6bf2fff64b8d292e117
Travis CI <travis@travis-ci.org>
2018-05-10 22:03:25 -0500

    Fix for missing version issue on distro builds

--- hack/distro.sh
+++ hack/distro.sh
@@ -24,6 +24,8 @@ S3_BASE_PATH="s3://kanalictl/release"
 echo $RELEASE >> latest.txt
 aws s3 mv latest.txt $S3_BASE_PATH/latest.txt
 
+BUILD_FLAGS=(-ldflags="-X $PATH_IMPORT/pkg/version.version=${RELEASE} -X $PATH_IMPORT/pkg/version.commit=${GIT_COMMIT}")
+
 for distro in ${DISTRIBUTIONS[@]}
 do
     remote_path="${S3_BASE_PATH}/${RELEASE}/${distro}/${PROJECT_NAME}ctl"
@@ -32,7 +34,7 @@ do
     desired_delimiter=_
     gox_binary_name=$(echo "${raw_binary_name}" | sed s/$actual_delimiter/$desired_delimiter/)
 
-    gox -osarch=$distro ${PATH_IMPORT}/cmd/kanalictl
+    gox "${BUILD_FLAGS[@]}" -osarch=$distro ${PATH_IMPORT}/cmd/kanalictl
 
     if [[ $distro = *"windows"* ]]; then
       aws s3 mv ${gox_binary_name}.exe ${remote_path}.exe
