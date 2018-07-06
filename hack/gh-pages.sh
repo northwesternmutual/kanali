6071f4b99fb8c328e8fc8800c1dfc32733a38961
Pfremmer, Bryan <bryanpfremmer@gmail.com>
2018-07-06 14:50:09 -0500

    Publishing to gh-pages-static-gen (publish.sh)

--- hack/gh-pages.sh
+++ hack/gh-pages.sh
@@ -7,22 +7,19 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+git worktree add -B gh-pages-static-gen public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
+git submodule update
 cd hugo
 hugo
 cd ..
 
-echo "Adding CNAME entry"
-cd public
-echo "kanali.io" >> CNAME
-
 echo "Updating gh-pages branch"
-git add -f --all && git commit -m "Publishing to gh-pages (publish.sh)"
+git add -f --all && git commit -m "Publishing to gh-pages-static-gen (publish.sh)"
 
 echo "Ready to push"
 cd ..
